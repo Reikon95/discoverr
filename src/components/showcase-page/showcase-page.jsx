910646e7b01f55790adf27875e04afd2fd038e11
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-02-18 09:19:30 +0000

    simplified deal flow process

--- src/components/showcase-page/showcase-page.jsx
+++ src/components/showcase-page/showcase-page.jsx
@@ -1,15 +1,34 @@
 import { React, useState } from "react"
 import { Link } from "react-router-dom"
+import Button from "@material-ui/core/Button"
+import TextField from "@material-ui/core/TextField"
+import Dialog from "@material-ui/core/Dialog"
+import DialogActions from "@material-ui/core/DialogActions"
+import DialogContent from "@material-ui/core/DialogContent"
+import DialogContentText from "@material-ui/core/DialogContentText"
+import DialogTitle from "@material-ui/core/DialogTitle"
 import "./showcase-page.css"
-import Button from '@material-ui/core/Button';
 
 export default function ShowcasePage({ name, deals, socials }) {
+  const [open, setOpen] = useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
   // todo - make columns 2 wide when on mobile
 
   return (
     <>
       <h2>{name}'s Showcase</h2>
-      <img className="profile-image" src="https://i.insider.com/5e14563c855cc23d4d6f14f3?width=1136&format=jpeg"/>
+      <img
+        className="profile-image"
+        src="https://i.insider.com/5e14563c855cc23d4d6f14f3?width=1136&format=jpeg"
+      />
       <p>
         Support {name}'s content by getting offers from top brands - it's a win
         win!
@@ -17,14 +36,14 @@ export default function ShowcasePage({ name, deals, socials }) {
 
       <h4>Keep up with {name}</h4>
       <div className="socials-list">
-      {socials.map((social) => {
-        return(
-          <div className="socials-list-item">
-          <Link to={social.link}>{social.social}</Link>
-          </div>
-        )
-      })}
-</div>
+        {socials.map((social) => {
+          return (
+            <div className="socials-list-item">
+              <Link to={social.link}>{social.social}</Link>
+            </div>
+          )
+        })}
+      </div>
       <h4>My Deals</h4>
 
       <div className="deals-container">
@@ -34,14 +53,44 @@ export default function ShowcasePage({ name, deals, socials }) {
               <div>{deal.name} </div>
               <div>{deal.offer}</div>
 
-              {deal.showDeal ? deal.name + name + "2021" : <Button className="deal-button" variant="contained" color="primary" href="#contained-buttons">
-
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={handleClickOpen}
+                className="deal-button"
+              >
                 Get Deal!
-              </Button>}
+              </Button>
             </div>
           )
         })}
       </div>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Cristiano's Nike Deal</DialogTitle>
+
+        <DialogContent>
+          <img
+            src="https://i.pinimg.com/originals/32/23/36/322336e667f7c01bb95681c82cbd5684.jpg"
+            className="deal-image"
+          />
+          <DialogContentText>Deal Code: CRISTIANO2021</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="danger">
+            Cancel
+          </Button>
+          <Button color="primary">
+            <a href="https://www.nike.com/gb/" target="_blanl">
+              GO TO NIKE!
+            </a>
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   )
 }
