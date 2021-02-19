6a1273e6998e34efedf5323d9af860847429f391
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-02-19 17:11:46 +0000

    props

--- src/components/ui-lib/dialog-box.jsx
+++ src/components/ui-lib/dialog-box.jsx
@@ -8,7 +8,7 @@ import DialogContent from "@material-ui/core/DialogContent"
 import DialogContentText from "@material-ui/core/DialogContentText"
 import DialogTitle from "@material-ui/core/DialogTitle"
 
-export default function DialogBox() {
+export default function DialogBox({ img, dealCode, store }) {
   const [open, setOpen] = useState(false)
 
   const handleClickOpen = () => {
