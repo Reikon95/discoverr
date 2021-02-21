9e0dc9c2d8233988dc5b5e3fe20baf681752b98b
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-02-21 14:46:02 +0000

    inputs

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -7,6 +7,25 @@ export default function SignUp() {
         <h1>Welcome to Discoverr.</h1>
         <p>Tell us about you here. Remember, the more information the add, the better your profile will be!</p>
         <Input type="text" placeholder="Your Name" required={true}></Input>
+        
+        Socials
+
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+
+        Current Offers
+
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
+
         </>
     )
 }
