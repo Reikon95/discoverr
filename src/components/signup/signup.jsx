6d6c4b30c82f91d2f1c1e4a025101be7ab5f24f4
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-02-21 18:35:48 +0000

    signup flow

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Input } from '@material-ui/core';
+import { Input, InputLabel  } from '@material-ui/core';
 
 export default function SignUp() {
     return (
         <>
         <h1>Welcome to Discoverr.</h1>
         <p>Tell us about you here. Remember, the more information the add, the better your profile will be!</p>
-        <Input type="text" placeholder="Your Name" required={true}></Input>
-        
-        Socials
+        <InputLabel>Name: </InputLabel><Input type="text" placeholder="Your Name" required={true}></Input>
+        <p>Put the name you're best known by online</p>
+        <InputLabel>Age: </InputLabel><Input type="number" min="18"></Input>
 
-        <Input type="text" placeholder="Your Name" required={true}></Input>
+        <h2>Socials</h2>
+        <p>Fill out whichever public social media profiles you actively use.</p>
+        <InputLabel>Instagram</InputLabel><Input type="text" placeholder="Instagram" required={true}></Input>
         <Input type="text" placeholder="Your Name" required={true}></Input>
         <Input type="text" placeholder="Your Name" required={true}></Input>
         <Input type="text" placeholder="Your Name" required={true}></Input>
         <Input type="text" placeholder="Your Name" required={true}></Input>
         <Input type="text" placeholder="Your Name" required={true}></Input>
 
-        Current Offers
+        <h2>Current Offers</h2>
+        <p>To get started, please list the current sponsorship offers you can pass onto your loyal followers!</p>
 
         <Input type="text" placeholder="Your Name" required={true}></Input>
         <Input type="text" placeholder="Your Name" required={true}></Input>
