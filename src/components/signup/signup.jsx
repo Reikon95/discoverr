5eeed1f51a4e998e7b6a7c44fc6769f82a115839
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-02-21 14:27:56 +0000

    mat inputs

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Input } from '@material-ui/core';
 
 export default function SignUp() {
     return (
         <>
-        Welcome to Discoverr.
+        <h1>Welcome to Discoverr.</h1>
+        <p>Tell us about you here. Remember, the more information the add, the better your profile will be!</p>
+        <Input type="text" placeholder="Your Name" required={true}></Input>
         </>
     )
 }
