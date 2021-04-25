046301118f9879a4eb734297063f6d6efda9b06b
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-04-25 20:26:27 +0100

    signup process

--- src/components/signup/signup.tsx
+++ src/components/signup/signup.tsx
@@ -1,5 +1,10 @@
 import React from "react"
-import { Input, InputLabel } from "@material-ui/core"
+import {
+  Input,
+  InputLabel,
+  FormControl,
+  FormHelperText,
+} from "@material-ui/core"
 import "./signup.scss"
 import TextField from "@material-ui/core/TextField"
 import Autocomplete from "@material-ui/lab/Autocomplete"
@@ -7,6 +12,10 @@ import Button from "@material-ui/core/Button"
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 export default function SignUp() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("hit submit")
+  }
   return (
     <>
       <h1>Welcome to Discoverr.</h1>
@@ -14,30 +23,62 @@ export default function SignUp() {
         Tell us about you here. Remember, the more information the add, the
         better your profile will be!
       </p>
-      <Input type="text" placeholder="Your Name" required={true}></Input>
-      <p>Put the name you're best known by online</p>
-      <Input
-        type="number"
-        placeholder="Your Age"
-        min="18"
-        required={true}
-      ></Input>
-      <h2>Socials</h2>
-      <p>
-        Link whichever public social media profiles you actively use. All fields
-        are optional.
-      </p>
-      <div className="socials-wrapper">
-        <Input type="text" placeholder="Instagram" required={true}></Input>
-        <Input type="text" placeholder="Twitter" required={true}></Input>
-        <Input type="text" placeholder="TikTok" required={true}></Input>
-        <Input type="text" placeholder="Facebook" required={true}></Input>
-        <Input type="text" placeholder="Youtube" required={true}></Input>
-      </div>
-      <p>Now let's move on to list your best offers for your audience!</p>
-      <Link to="/signup2">
-        <Button>Next Step</Button>
-      </Link>
+      <form onSubmit={handleSubmit}>
+        <div className="form-vertical-batch">
+          <FormControl>
+            <Input type="text" placeholder="Your Name" required={true}></Input>
+
+            <FormHelperText>
+              Put the name you're best known by online
+            </FormHelperText>
+          </FormControl>
+          <FormControl>
+            <Input
+              type="number"
+              placeholder="Your Age"
+              min="18"
+              required={true}
+            ></Input>
+          </FormControl>
+        </div>
+        <h2>Socials</h2>
+        <p>
+          Link whichever public social media profiles you actively use. All
+          fields are optional.
+        </p>
+        <div className="socials-wrapper form-vertical-batch">
+          <FormControl>
+            <Input type="text" placeholder="Instagram" required={false}></Input>
+          </FormControl>
+
+          <FormControl>
+            <Input type="text" placeholder="Twitter" required={false}></Input>
+          </FormControl>
+
+          <FormControl>
+            <Input type="text" placeholder="TikTok" required={false}></Input>
+          </FormControl>
+
+          <FormControl>
+            <Input type="text" placeholder="Facebook" required={false}></Input>
+          </FormControl>
+
+          <FormControl>
+            <Input type="text" placeholder="Youtube" required={false}></Input>
+          </FormControl>
+        </div>
+
+        <p>Now let's move on to list your best offers for your audience!</p>
+        {/* <Link to="/signup2"> */}
+        {/* <Button>Next Step</Button> */}
+        <input
+          id="formButton"
+          className="btn"
+          type="submit"
+          placeholder="Send message"
+        />
+        {/* </Link> */}
+      </form>
     </>
   )
 }
