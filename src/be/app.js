5b907f01612b65d806ecd43aee51d296e7d9175f
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-05-19 21:40:59 +0100

    backend setup

--- src/be/app.js
+++ src/be/app.js
@@ -1,22 +1,27 @@
 const express = require("express")
 const cors = require("cors")
-const app = express()
-const morgan = require("morgan")
 const mongoose = require("mongoose")
-const port = process.env.PORT || 3003
 
-const dbURI =
-  "mongodb+srv://cameron:Amber12345@discoverr0.tkn07.mongodb.net/discoverrusers?retryWrites=true&w=majority"
+require("dotenv").config()
 
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => app.listen(port))
-  .catch((err) => console.log(err))
+const app = express()
+const port = process.env.PORT || 5000
 
-// const getProducts = require("./Products.js")
 app.use(cors())
-app.get("/", (req, res) => res.send("Hello Node!"))
-// app.get("/rest/products", (req, res) => {
-//   let products = getProducts()
-//   res.send(products)
-// })
+app.use(express.json())
+
+const uri =
+  "mongodb+srv://cameron:Amber12345@discoverr0.tkn07.mongodb.net/discoverrusers?retryWrites=true&w=majority"
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+const connection = mongoose.connection
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully")
+})
+
+const usersRouter = require("./routes/users")
+
+app.use("/users", usersRouter)
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`)
+})
