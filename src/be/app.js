50e6b082517b8ee1684c9f8a73ec3e420d453084
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-05-19 21:57:02 +0100

    hid keys

--- src/be/app.js
+++ src/be/app.js
@@ -10,8 +10,8 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri =
-  "mongodb+srv://cameron:Amber12345@discoverr0.tkn07.mongodb.net/discoverrusers?retryWrites=true&w=majority"
+const uri = process.env.ATLAS_URI
+
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true })
 const connection = mongoose.connection
 connection.once("open", () => {
