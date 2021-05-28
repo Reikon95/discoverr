d86576c111474d19b1675396d1de83b1ea849309
Cameron Blackwood <cameronjwblackwood@gmail.com>
2021-05-28 18:04:50 +0100

    notes

--- src/be/routes/users.js
+++ src/be/routes/users.js
@@ -9,10 +9,12 @@ router.route("/").get((req, res) => {
 })
 
 router.route("/adduser").post((req, res) => {
+  console.log(req, res)
+  const reqBody = req.body
   const userID = req.body.id
   const userName = req.body.name
   const userBio = req.body.bio
-  const newUser = new User({ id: userID, name: userName, bio: userBio })
+  const newUser = new User({ id: userID, name: userName, bio: userBio, googleId: reqBody.googleId })
 
   newUser
     .save()
@@ -20,4 +22,21 @@ router.route("/adduser").post((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err))
 })
 
+router.route("/addgoogleuser").post((req, res) => {
+  console.log(req, res)
+
+  // new google route, this needs to check for a duplicate (ideally via email)
+  // other than that, goes through normal process
+  // possibly, the check should happen BEFORE the post request #
+  // takes place, so we aren't making unneeded calls.
+
+  // newUser
+  //   .save()
+  //   .then(() => res.json("User registered"))
+  //   .catch((err) => res.status(400).json("Error: " + err))
+})
+
 module.exports = router
+
+
+// new route????
